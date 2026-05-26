14ca0fa195a34f1bb15e8b2138feb00ad0834a29
TOM ROY THOMAS <tomroy246@gmail.com>
2026-05-26 18:02:21 +0530

    Initial luxury Varnika website

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", () => {
+
+  const navbar = document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+
+    navbar.style.background = "#050505";
+
+  }
+
+  else{
+
+    navbar.style.background = "rgba(0,0,0,0.2)";
+
+  }
+
+});
